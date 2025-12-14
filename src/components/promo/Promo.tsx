16f305c0e8083e432701596f2e3e86c270dfd04e
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2025-12-14 10:40:34 +0100

    feat: finish promo section layout and add mixins

--- src/components/promo/Promo.tsx
+++ src/components/promo/Promo.tsx
@@ -1,11 +1,15 @@
 import "./Promo.scss";
 import Header from "../header/Header";
+import vaseImg from "/src/assets/img/promo/ceramic-vase.jpg";
 
 const Promo = () =>{
     return(
         <section className="promo">
             <div className="container">
                 <Header/>
+                <h1 className="title-h1-italic promo__title">Make your dream come true or decorate your home</h1>
+                <img src={vaseImg} alt="vase" className="promo__img" />
+                <button className="btn-default promo__btn">shop now</button>
             </div>
         </section>
     )

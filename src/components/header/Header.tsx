0bfbe270b420d278520a46959f8cad360bd8dff7
Nazar Stepaniuk <annastepaniuk77@gmail.com>
2025-12-18 16:31:07 +0100

    feat: add responsive styles to promo section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,12 +3,21 @@ import "./Header.scss";
 const Header = () =>{
     return(
         <header className="header">
-            <div className="burger"><span></span><span></span><span></span></div>
-            <a href="#" className="header__logo"><img src="src/assets/logo/logo.png" alt="logo" /></a>
-            <div className="header__icons">
-                <a href="#"><span className="icon-search"></span></a>
-                <a href="#"><span className="icon-shopping-bag"></span></a>
+            <div className="header__top">
+                <div className="burger"><span></span><span></span><span></span></div>
+                <a href="#" className="header__logo"><img src="src/assets/logo/logo.png" alt="logo" /></a>
+                <div className="header__icons">
+                    <a href="#"><span className="icon-search"></span></a>
+                    <a href="#"><span className="icon-shopping-bag"></span></a>
+                </div>
             </div>
+            <nav className="header__nav">
+                <ul>
+                    <li><a href="" className="header__link">catalog</a></li>
+                    <li><a href="" className="header__link">blog</a></li>
+                    <li><a href="" className="header__link">about</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
--- src/components/promo/Promo.tsx
+++ src/components/promo/Promo.tsx
@@ -1,15 +1,21 @@
 import "./Promo.scss";
 import Header from "../header/Header";
 import vaseImg from "/src/assets/img/promo/ceramic-vase.jpg";
+import potterImg from "/src/assets/img/promo/potter.jpg";
 
 const Promo = () =>{
     return(
         <section className="promo">
+            <img src={potterImg} alt="potter" className="promo__potter" />
             <div className="container">
                 <Header/>
-                <h1 className="title-h1-italic promo__title">Make your dream come true or decorate your home</h1>
-                <img src={vaseImg} alt="vase" className="promo__img" />
-                <button className="btn-default promo__btn">shop now</button>
+                <div className="promo__wrapper">
+                    <h1 className="title-h1-italic promo__title">Make your dream come true<br/>or decorate your home</h1>
+                    <div className="promo__cta"> 
+                        <img src={vaseImg} alt="vase" className="promo__img" />
+                        <button className="btn-default promo__btn">shop now</button>
+                    </div>
+                </div>
             </div>
         </section>
     )
